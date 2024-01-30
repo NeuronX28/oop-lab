aa121f4508fe0c94dd95944bb79b78793bbcefb4
vishweshwar <92378118+NeuronX28@users.noreply.github.com>
2024-01-30 11:35:23 +0530

    Update rev.java

--- week2/rev.java
+++ week2/rev.java
@@ -1,38 +1,33 @@
-
 import java.util.Scanner;
+class Reverse{
+	public static void main (String args[]) {
+ 	Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+		int [] arr = new int[size];
+		int [] arr2=  new int[size];
+		System.out.println("input"+""+size+ "intergers");
+		for(int i=0; i<size; i++)
+			{
+			System.out.print("Element " + (i + 1) + ": ");
+			arr[i] = sc.nextInt();
+			int rev =0;
+			int temp = arr[i];
+		    while(temp !=0)
+				 {
+					int remainder = temp%10;
+					rev = rev *10 + remainder;
+					temp/=10;
+				 }
+				   arr2[i]= rev;
+			}
 
-class Array {
-    public static void main(String args[]) {
-        int n, j;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter no of elements: ");
-        n = sc.nextInt();
-        int arr[], arr1[];
-        arr = new int[n];
-        arr1 = new int[n];
+		    System.out.println("ori array and  rev Array:");
+			for (int i =0; i<size ; i++)
+				{
+				System.out.println(arr[i] + " \t" + arr2[i] + " ");
+		        }
 
-        System.out.println("Enter Elements:");
-        for (int i = 0; i < n; i++) {
-            j = sc.nextInt();
-            arr[i] = j;
-        }
+  }
 
-       int[] temp = arr.clone();
-
-        for (int i = 0; i < n; i++) {
-            // Reverse the number
-            int reversedNum = 0;
-            while (temp[i] != 0) {
-                int digit = temp[i] % 10;
-                reversedNum = reversedNum * 10 + digit;
-                temp[i] = temp[i] / 10;
-            }
-            arr1[i] = reversedNum;
-        }
-
-        System.out.println("Original\tReversed ");
-        for (int i = 0; i < n; i++) {
-            System.out.println(arr[i] + "\t" + arr1[i]);
-        }
-    }
 }
